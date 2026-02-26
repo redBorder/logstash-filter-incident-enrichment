1c2de1942ae30d7f483d7e0288d0bbdd939d3593
flopez <flopez@redborder.com>
2026-02-26 10:35:45 +0000

    Change Uknown incident name in Vault incidents

--- lib/logstash/filters/incident_enrichment.rb
+++ lib/logstash/filters/incident_enrichment.rb
@@ -135,10 +135,13 @@ class LogStash::Filters::IncidentEnrichment < LogStash::Filters::Base
   end
 
   def get_name(event)
-    if @source == 'Malware'
+    case @source
+    when 'Malware'
       "Malware detected in #{event.get('file_name') || 'unknown file'}"
+    when 'Vault'
+      "Syslog incident from sensor #{event.get('sensor_name')}"
     else
-      event.get(MSG) || 'Unknown incident'
+      event.get(MSG) || 'Syslog incident from sensor '
     end
   end
 
